818d2fe689f05d0640089075962f2e8afec62a40
Nigel Tatschner <ntatschner@gmail.com>
2019-04-11 11:24:46 +0100

    Adding missing function

--- PSCode/Azure_VM_Slack_Info.ps1
+++ PSCode/Azure_VM_Slack_Info.ps1
@@ -30,6 +30,34 @@ else
 $SubscriptionID = Get-AutomationVariable -Name 'SubscriptionID'
 $emailAddress = $webhookbody.email
 
+# Functions
+
+function ConvertFrom-VMTagJSON
+{
+	[CmdletBinding()]
+	[OutputType([object])]
+	param
+	(
+		[Parameter(Mandatory = $true,
+				   ValueFromPipeline = $true,
+				   HelpMessage = 'The Tag where the value is JSON')]
+		[ValidateNotNullOrEmpty()]
+		[System.String]$JSONTag
+	)
+	
+	Process
+	{
+		try
+		{
+			$JSONTag | ConvertFrom-Json
+		}
+		Catch
+		{
+			Write-Error "Unable to convert Tag Input to Object from JSON. Error: $($_.Exception.Message)"
+		}
+	}
+}
+
 # Azure Connection
 # Connect to AzureRM
 $connection = Get-AutomationConnection -Name AzureRunAsConnection
